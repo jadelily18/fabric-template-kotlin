a32d095c53ffee8f99541f3937eca9e737596155
Jade Nash <jade.nashh18@gmail.com>
2022-11-02 15:04:40 -0500

    basic setup

--- src/main/java/com/lilydev/example_mod/mixin/ExampleMixin.java
+++ src/main/java/com/lilydev/example_mod/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package com.lilydev.example_mod.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import com.lilydev.example_mod.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		ExampleMod.LOGGER.info("This line is printed by an {} mixin!", ExampleMod.MOD_NAME);
 	}
 }
